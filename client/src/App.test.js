0208d538e96fa4d6e3dfd1d1ae24f1692d601564
kkdev211 <kkdev211@users.noreply.github.com>
2020-03-22 23:06:47 -0400

    tests

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as rtl from '@testing-library/react';
 import App from './App';
+import Navbar from "./components/NavBar";
+import PlayerCard from './components/PlayerCard';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
+  //app renders to dom
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+//nav bar renders to dom
+test('renders Navbar without crashing', () => {
+  rtl.render(<Navbar />);
+});
+
+//PlayerCard renders
+test('renders PlayerCard without crashing', () => {
+  const player = {
+    "name": "test name",
+    "country": "test country",
+    "searches": "test searches"
+  }
+  rtl.render(<PlayerCard player={player}/>);
 });
